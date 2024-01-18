c2d3d6a69b7c8c5fff55ecdf7de6df6792da7fa3
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2024-01-18 13:13:19 -0500

    commit 6

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const DOMSelectors = {
   container: document.querySelector(".container"),
+  buttons: document.querySelector(".buttons"),
 };
-const array = [];
+/* const array = [];
 for (let i = 0; i <= 255; i++) {
   array.push(i);
 }
-
-console.log(array);
+ */
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -22,14 +22,14 @@ DOMSelectors.container.insertAdjacentHTML(
   </div>`
 );
 
-function rgbt() {
+function rgb() {
   let a = Math.round(Math.random() * 255);
   let b = Math.round(Math.random(1) * 255);
   let c = Math.round(Math.random(2) * 255);
-  document.body.style.backgroundColor = (a, b, c);
+  document.getElementsByClassName("buttons").style.backgroundColor = (a, b, c);
 }
-console.log(rgbt());
+console.log(rgb());
 DOMSelectors.container.addEventListener("click", function (e) {
   e.preventDefault();
-  rgbt();
+  rgb();
 });
