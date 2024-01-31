9264e30ff39d1dd502abe32e22e2090a9e0e50ab
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2024-01-31 13:16:50 -0500

    commit 9

--- main.js
+++ main.js
@@ -4,15 +4,6 @@ const DOMSelectors = {
   button: document.querySelector("#button1"),
 };
 
-/* DOMSelectors.container.insertAdjacentHTML(
-  "beforeend",
-  `<div class=card>
-  <h1>${a}</h1>
-  <h1>${b}</h1>
-  <h1>${c}</h1>
-  </div>`
-);
- */
 function rgb() {
   let a = Math.round(Math.random() * 256);
   let b = Math.round(Math.random() * 256);
@@ -20,15 +11,31 @@ function rgb() {
   return { a, b, c };
 }
 
-event.preventDefault();
-rgb();
-button.style.backgroundColor = (a, b, c);
-
+console.log(DOMSelectors.button);
+function changeBackground() {
+  let x = rgb();
+  DOMSelectors.button.style.backgroundColor = (x.a, x.b, x.c);
+  document.querySelector(".guess").style.backgroundColor = (x.a, x.b, x.c);
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class=card>
+    <h1>${x.a}</h1>
+    <h1>${x.b}</h1>
+    <h1>${x.c}</h1>
+    </div>`
+  );
+}
+changeBackground();
 //store 1-4 in array and swap order
 const array = [];
-for (let i = 0; i <= 4; i++) {
+for (let i = 1; i <= 4; i++) {
   array.push(i);
 }
 
+/* function switchButtonOrder() {}
 console.log(array);
-DOMSelectors.button.addEventListener("click", rgb);
+DOMSelectors.button.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(changeBackground());
+});
+ */
